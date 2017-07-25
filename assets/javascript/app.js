d1e28519e486e869516d2a698cc065e0c9f069c8
Abbey Hudson <a_hudson2@yahoo.com>
2017-07-25 06:40:34 -0700

    check question code and answer page added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,55 +1,95 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  var time = 100;
+  var intervalId;
+  var correctAnswers = 0;
+  var incorrectAnswers = 0;
 
-	var time = 200;
-	var intervalId;
-	var correctAnswers = 0;
-	var incorrectAnswers = 0;
-	var unansweredQuestions = 0;
-	var questions
+  $(".question-page").hide();
+  $(".answer-page").hide();
 
-		$(".question-page").hide();
-		$(".answer-page").hide();
+  $("#start").on("click", function() {
+    $(".start-page").hide();
+    $(".question-page").show();
+    $(".answer-page").hide();
+    run();
+  });
 
-	$("#start").on("click", function() {
-		$(".start-page").hide();
-		$(".question-page").show();
-		$(".answer-page").hide();
-		run();
-	});
-
-	function run() {
+  function run() {
     intervalId = setInterval(decrement, 1000);
-   }
+  }
 
   function decrement() {
     time--;
     $("#countDown").html(time);
   }
 
+  function checkQuestion() {
+    if (document.getElementById("right-answer").checked) {
+      correctAnswers++;
+    } else {
+      incorrectAnswers++;
+    }
 
+    if (document.getElementById("right-answer2").checked) {
+      correctAnswers++;
+    } else {
+      incorrectAnswers++;
+    }
 
+    if (document.getElementById("right-answer3").checked) {
+      correctAnswers++;
+    } else {
+      incorrectAnswers++;
+    }
 
-      //  Once number hits zero...
-      //if (time) === 0)
+    if (document.getElementById("right-answer4").checked) {
+      correctAnswers++;
+    } else {
+      incorrectAnswers++;
+    }
 
+    if (document.getElementById("right-answer5").checked) {
+      correctAnswers++;
+    } else {
+      incorrectAnswers++;
+    }
 
-//set up timer
-	//if timer runs out, move to answer page
+    if (document.getElementById("right-answer6").checked) {
+      correctAnswers++;
+    } else {
+      incorrectAnswers++;
+    }
 
-//for each question
-	//register answer
-	//set right answer
-	//if answer is correct, write to correct
-	//if answer is incorrect, write to incorrect
-	//if no answer is chosen, write to unanswered
+    if (document.getElementById("right-answer7").checked) {
+      correctAnswers++;
+    } else {
+      incorrectAnswers++;
+    }
 
-//function check() {
-    //document.getElementById("red").checked = true;}
-//function uncheck() {
-   // document.getElementById("red").checked = false;}
+    if (document.getElementById("right-answer8").checked) {
+      correctAnswers++;
+    } else {
+      incorrectAnswers++;
+    }
 
-   //write correct, incorrect, unanswered to answer page
+    var unansweredQuestions = 8 - correctAnswers - incorrectAnswers;
 
-   //restart button (back to question page)
+    $("#correct-answers").html(correctAnswers);
 
+    $("#wrong-answers").html(incorrectAnswers);
+  }
+
+  $("#done").on("click", function() {
+    checkQuestion();
+    $(".start-page").hide();
+    $(".question-page").hide();
+    $(".answer-page").show();
+  });
+
+  if (time === 0) {
+    checkQuestion();
+    $(".start-page").hide();
+    $(".question-page").hide();
+    $(".answer-page").show();
+  }
 });
